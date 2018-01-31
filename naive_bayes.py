860fb8cada3d3366154e6034d340b6ea96a2f1db
G Roques <groques360@gmail.com>
2018-01-31 06:07:25 -0600

    Load CSV data from file

---
+++ naive_bayes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+Naive bayes implementation in Python from scratch.
+
+Adapted from Jason Brownlee at Machine Learning Mastery.
+Source: https://machinelearningmastery.com/naive-bayes-classifier-scratch-python/
+"""
+
+import csv
+
+def main():
+    filename = 'pima-indians-diabetes.csv'
+    dataset = load_csv(filename)
+    print('Loaded data file {0} with {1} rows'.format(filename, len(dataset)))
+
+def load_csv(filename):
+    """Load csv data from a file and convert attributes to floats."""
+    file = open(filename)
+    lines = csv.reader(file)
+    dataset = list(lines)
+    for i in range(len(dataset)):
+        dataset[i] = [float(x) for x in dataset[i]]
+    return dataset
+
+if __name__ == '__main__':
+    main()
