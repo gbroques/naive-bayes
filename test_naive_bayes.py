9bbd602a2a6b1a674e520a4ab76e84e00ce3285e
G Roques <groques360@gmail.com>
2018-02-02 05:53:47 -0600

    Refactor tests to not depend upon external CSV file

--- test_naive_bayes.py
+++ test_naive_bayes.py
@@ -1,21 +1,40 @@
 import unittest
+import csv
+import os
 import naive_bayes
+
+test_data = [[6, 148, 72, 35, 0, 33.6, 0.627, 50, 1],
+             [1, 85,  66, 29, 0, 26.6, 0.351, 31, 0],
+             [8, 183, 64, 0 , 0, 23.3, 0.672, 32, 1]]
 
 class TestNaiveBayes(unittest.TestCase):
 
+    filename = 'test.csv'
+
+    @classmethod
+    def setUpClass(cls):
+        cls.create_csv(cls.filename)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(cls.filename)
+
+    @classmethod
+    def create_csv(cls, filename):
+        with open(filename, 'w') as file:
+            writer = csv.writer(file)
+            for row in test_data:
+                writer.writerow(row)
+
     def test_load_csv_file(self):
-        filename = 'pima-indians-diabetes.csv'
-        dataset = naive_bayes.load_csv(filename)
+        dataset = naive_bayes.load_csv(self.filename)
 
-        num_rows = len(dataset)
-        self.assertEqual(num_rows, 768)
+        self.assertEqual(len(dataset), len(test_data))
 
-        last_row = [1, 93, 70, 31, 0, 30.4 , 0.315, 23, 0]
-        self.assertEqual(dataset[num_rows - 1], last_row)
+        self.assertEqual(dataset[-1], test_data[-1])
 
     def test_split_dataset(self):
-        filename = 'pima-indians-diabetes.csv'
-        dataset = naive_bayes.load_csv(filename)
+        dataset = naive_bayes.load_csv(self.filename)
 
         split_ratio = 0.67
         train, test = naive_bayes.split_dataset(dataset, split_ratio)
